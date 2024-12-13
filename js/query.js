61f905a8826cddd618fac10ad08aab67aa78aaac
Nur Jahan Nishy <nishyrahman243@gmail.com>
2024-12-13 21:54:33 +0600

    Add files via upload

---
+++ js/query.js
@@ -0,0 +1,10 @@
+@media screen and (max-width: 768px) {
+    .institution {
+      flex-direction: column;
+      text-align: center;
+    }
+  
+    .info-section {
+      text-align: center;
+    }
+  }
